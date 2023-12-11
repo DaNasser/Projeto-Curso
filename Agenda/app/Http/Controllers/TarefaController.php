e329a7013c65e8a1a842261ab3c049ea090db791
BernardoGuerino <bernardo.coelho.guerino@gmail.com>
2023-12-11 20:42:18 -0300

    .

--- Agenda/app/Http/Controllers/TarefaController.php
+++ Agenda/app/Http/Controllers/TarefaController.php
@@ -1,17 +1,49 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Tarefa;
+use App\resources\views\auth;
 
 use Illuminate\Http\Request;
 
 class TarefaController extends Controller
 {
    
+ public function index(){
+    $tarefa  = tarefa::where('user_id',Auth()->user()->id);
+  return view('calendario', compact('tarefa'));
+ }
+
+ public function create(){
+    return view('');
+   }
+ public function store(Request $requer){
+    $tarefa = new tarefa();
+    $tarefa->nome = $requer->input('nome');
+    $tarefa->data = $requer->input('data');
+    $tarefa->user_id = Auth()->user()->id;
+    $tarefa->save();
+
     
+   }
 
+public function edit($id){
+    $tarefa = tarefa::find($id);
+    return view('');
+   }
 
-    public function Tarefa()
-    {
-        return view('tarefa');
-    } 
+public function update(Request $requer,$id){
+    $tarefa =  tarefa::find($id);
+    $tarefa->nome = $requer->input('nome');
+    $tarefa->data = $requer->input('data');
+    $tarefa->update();
+    
+   }
+
+   public function deletar($id){
+    $tarefa =  tarefa::find($id);
+    $tarefa->delete();
+
+   }
+
 }
--- Agenda/resources/views/calendario.blade.php
+++ Agenda/resources/views/calendario.blade.php
@@ -1,262 +1,71 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Calendario </title>
-
-	<meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
-
-<link href='assets/css/fullcalendar.css' rel='stylesheet' />
-<link href='assets/css/fullcalendar.print.css' rel='stylesheet' media='print' />
-<script src='assets/js/jquery-1.10.2.js' type="text/javascript"></script>
-<script src='assets/js/jquery-ui.custom.min.js' type="text/javascript"></script>
-<script src='assets/js/fullcalendar.js' type="text/javascript"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<script>
-
-	$(document).ready(function() {
-	    var date = new Date();
-		var d = date.getDate();
-		var m = date.getMonth();
-		var y = date.getFullYear();
-
-		/*  className colors
-
-		className: default(transparent), important(red), chill(pink), success(green), info(blue)
-
-		*/
-
-
-		/* initialize the external events
-		-----------------------------------------------------------------*/
-
-		$('#external-events div.external-event').each(function() {
-
-			// create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
-			// it doesn't need to have a start or end
-			var eventObject = {
-				title: $.trim($(this).text()) // use the element's text as the event title
-			};
-
-			// store the Event Object in the DOM element so we can get to it later
-			$(this).data('eventObject', eventObject);
-
-			// make the event draggable using jQuery UI
-			$(this).draggable({
-				zIndex: 999,
-				revert: true,      // will cause the event to go back to its
-				revertDuration: 0  //  original position after the drag
-			});
-
-		});
-
-
-		/* initialize the calendar
-		-----------------------------------------------------------------*/
-
-		var calendar =  $('#calendar').fullCalendar({
-			header: {
-				left: 'title',
-				center: 'agendaDay,agendaWeek,month',
-				right: 'prev,next today'
-			},
-			editable: true,
-			firstDay: 1, //  1(Monday) this can be changed to 0(Sunday) for the USA system
-			selectable: true,
-			defaultView: 'month',
-
-			axisFormat: 'h:mm',
-			columnFormat: {
-                month: 'ddd',    // Mon
-                week: 'ddd d', // Mon 7
-                day: 'dddd M/d',  // Monday 9/7
-                agendaDay: 'dddd d'
-            },
-            titleFormat: {
-                month: 'MMMM yyyy', // September 2009
-                week: "MMMM yyyy", // September 2009
-                day: 'MMMM yyyy'                  // Tuesday, Sep 8, 2009
-            },
-			allDaySlot: false,
-			selectHelper: true,
-			select: function(start, end, allDay) {
-				var title = prompt('Nome de Evento:');
-				if (title) {
-					calendar.fullCalendar('renderEvent',
-						{
-							title: title,
-							start: start,
-							end: end,
-							allDay: allDay
-						},
-						true // make the event "stick"
-					);
-				}
-				calendar.fullCalendar('unselect');
-			},
-			droppable: true, // this allows things to be dropped onto the calendar !!!
-			drop: function(date, allDay) { // this function is called when something is dropped
-
-				// retrieve the dropped element's stored Event Object
-				var originalEventObject = $(this).data('eventObject');
-
-				// we need to copy it, so that multiple events don't have a reference to the same object
-				var copiedEventObject = $.extend({}, originalEventObject);
-
-				// assign it the date that was reported
-				copiedEventObject.start = date;
-				copiedEventObject.allDay = allDay;
-
-				// render the event on the calendar
-				// the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
-				$('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
-
-				// is the "remove after drop" checkbox checked?
-				if ($('#drop-remove').is(':checked')) {
-					// if so, remove the element from the "Draggable Events" list
-					$(this).remove();
-				}
-
-			},
-
-			events: [
-				{
-					title: 'Christmas',
-					start: new Date(2023, 12, 23)
-				},
-				{
-					id: 999,
-					title: 'easter',
-					start: new Date(2024, 3, 20),
-					className: 'info'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: new Date(y, m, d+4, 16, 0),
-					allDay: false,
-					className: 'info'
-				},
-				{
-					title: 'Meeting',
-					start: new Date(y, m, d, 10, 30),
-					allDay: false,
-					className: 'important'
-				},
-				{
-					title: 'Lunch',
-					start: new Date(y, m, d, 12, 0),
-					end: new Date(y, m, d, 14, 0),
-					allDay: false,
-					className: 'important'
-				},
-				{
-					title: 'Birthday Party',
-					start: new Date(y, m, d+1, 19, 0),
-					end: new Date(y, m, d+1, 22, 30),
-					allDay: false,
-				},
-				{
-					title: 'Click for Google',
-					start: new Date(y, m, 28),
-					end: new Date(y, m, 29),
-					url: 'http://google.com/',
-					className: 'success'
-				}
-			],
-		});
-
-
-	});
-
-</script>
-<style>
-
-	body {
-		margin-top: 40px;
-		text-align: center;
-		font-size: 14px;
-		font-family: "Helvetica Nueue",Arial,Verdana,sans-serif;
-		background-color: #DDDDDD;
-		}
-
-	#wrap {
-		width: 1100px;
-		margin: 0 auto;
-		}
-
-	#external-events {
-		float: left;
-		width: 150px;
-		padding: 0 10px;
-		text-align: left;
-		}
-
-	#external-events h4 {
-		font-size: 16px;
-		margin-top: 0;
-		padding-top: 1em;
-		}
-
-	.external-event { /* try to mimick the look of a real event */
-		margin: 10px 0;
-		padding: 2px 4px;
-		background: #3366CC;
-		color: #fff;
-		font-size: .85em;
-		cursor: pointer;
-		}
-
-	#external-events p {
-		margin: 1.5em 0;
-		font-size: 11px;
-		color: #666;
-		}
-
-	#external-events p input {
-		margin: 0;
-		vertical-align: middle;
-		}
-
-	#calendar {
-/* 		float: right; */
-        margin: 0 auto;
-		width: 900px;
-		background-color: #FFFFFF;
-		  border-radius: 6px;
-        box-shadow: 0 1px 2px #C3C3C3;
-		}
-
-		.salvar{
-		margin-top:1%; 
-		margin-left: 10%; 
-		margin-right: 500%;
-		}
-
-</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-<div id='wrap'>
+    <div id='wrap'>
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Tarefa</th>
+                    <th scope="col">Data</th>
+                    <th scope="col">Grau de Prioridade</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tarefa as $tarefa)
+                <td>{{$tarefa->nome}}</td> 
+                <td>{{$tarefa->data}}</td> 
 
-<div id='calendar'></div>
+                @endforeach
+             
+            </tbody>
+        </table>
 
-<div style='clear:both'></div>	
+        <button class="btn btn-primary" onclick="addTask()">Adicionar Tarefa</button>
+    </div>
 
+    <script>
+        let taskCounter = 1;
 
-</div>
-<label class= "Salvar">
-	<a href= 'http://127.0.0.1:8000/criar/tarefa'><button type="button" class="btn btn-outline-danger">Adicionar Tarefa</button></a>
-</label>
+        function addTask() {
+            const tableBody = document.getElementById('tasksTableBody');
 
-</div>
-<a href= "{{route('logout')}}"><button type="button" class="btn btn-outline-dark">Sair</button></a>
-<div>
-</label>
+            const newRow = document.createElement('tr');
+            newRow.innerHTML = `
+                <th scope="row">${taskCounter}</th>
+                <td contenteditable="true">Nova Tarefa</td>
+                <td contenteditable="true">Nova Data</td>
+                <td contenteditable="true">Nova Prioridade</td>
+                <td>
+                    <button class="btn btn-warning" onclick="editTask(this)">Editar</button>
+                    <button class="btn btn-danger" onclick="deleteTask(this)">Excluir</button>
+                </td>
+            `;
 
-</div>
+            tableBody.appendChild(newRow);
+            taskCounter++;
+        }
 
+        function editTask(button) {
+            const row = button.closest('tr');
+            const cells = row.querySelectorAll('td[contenteditable="true"]');
+
+            cells.forEach(cell => {
+                cell.setAttribute('contenteditable', 'true');
+            });
+        }
+
+        function deleteTask(button) {
+            const row = button.closest('tr');
+            row.remove();
+        }
+    </script>
 </body>
-</html>
-<div></div>
